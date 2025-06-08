329349e56e98b5ba33aa236bb5d70c19135b1906
Andrea Rossi <andrea.rossi0507@gmail.com>
2025-06-09 03:51:15 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
 import Navbar from "@/components/ui/navbar";
+import { HeroUIProvider } from "@heroui/system";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Navbar />
-        {children}
+        <HeroUIProvider>
+          <Navbar />
+          {children}
+        </HeroUIProvider>
       </body>
     </html>
   );
